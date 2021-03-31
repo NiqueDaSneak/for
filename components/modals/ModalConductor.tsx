a2f7ba61255d8a877737e0ec881a28fef94c22cf
niquedasneak <thisisdom4@gmail.com>
2021-03-31 11:36:16 -0400

    style fix and add newuserhelpmodal

--- components/modals/ModalConductor.tsx
+++ components/modals/ModalConductor.tsx
@@ -1,18 +1,30 @@
 import React, {useContext} from 'react'
 import { ModalContext } from '../../state'
 import PhoneVerification from './PhoneVerification'
+import NewUserHelpModal from './NewUserHelpModal'
 
 const ModalConductor = ({colorScheme}: {colorScheme: string}) => {
   const [state, dispatch] = useContext(ModalContext)
   const {
-    modalType, modalData
+    modalType,
+    modalData,
+    showNewUserHelpModal
   } = state
 
   const closeModal = () => {
     dispatch({type: 'CLOSE'})
   }
   return (
-    <PhoneVerification close={() => closeModal()} colorScheme={colorScheme} visible={modalType === 'GET_PHONE'} />
+    <>
+      <PhoneVerification
+        visible={modalType === 'GET_PHONE'}
+        colorScheme={colorScheme}
+        close={() => closeModal()} />
+      <NewUserHelpModal
+        visible={modalType === 'SHOW_NEW_HELP'}
+        colorScheme={colorScheme}
+        close={() => closeModal()}/>
+    </>
   )
  }
 
