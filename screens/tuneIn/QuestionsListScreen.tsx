59c79b78554f79cef555d5dfcca041173861fe43
niquedasneak <thisisdom4@gmail.com>
2021-04-05 23:05:35 -0400

    update question list

--- screens/tuneIn/QuestionsListScreen.tsx
+++ screens/tuneIn/QuestionsListScreen.tsx
@@ -4,6 +4,7 @@ import SegmentedControl from '@react-native-segmented-control/segmented-control'
 import { PlatformColor, Pressable, SectionList, useColorScheme } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { BlurView } from 'expo-blur';
+import PressableTextCard from '../../components/PressableTextCard'
 
 const QuestionsListScreen = ({ navigation, route }) => {
   const {sectionTitle} = route.params
@@ -13,7 +14,61 @@ const QuestionsListScreen = ({ navigation, route }) => {
   useEffect(() => {
     navigation.setOptions({title: `${sectionTitle} Questions`})
   }, [])
-
+  
+      const questionSectionData = [
+    {
+      title: 'Intentions for change:',
+      data: [
+        {
+          text: 'What do you intend to do?'
+        },
+        {
+          text: 'What do you think you might be able to do?'
+        },
+      ]
+    },
+    {
+      title: 'Disadvantages of the status quo:',
+      data: [
+        {
+          text: 'What concerns you about your current situation?'
+        },
+        {
+          text: 'What do you think might happen if you do not change?'
+        }
+      ]
+    },
+    {
+      title: 'Expressing optimism:',
+      data: [
+        {
+          text: 'How confident are you that you can make this change?'
+        },
+        {
+          text: 'What kind of support would be helpful in making this change?'
+        },
+        {
+          text: 'What kind of support would be helpful in making this change?'
+        }
+      ]
+    },
+    {
+      title: 'Advantages of change:',
+      data: [
+        {
+          text: 'If you could wake up tomorrow and things changed by magic, how would things be better for you?'
+        }
+      ]
+    },
+    {
+      title: 'Scaling:',
+      data: [
+        {
+        text: 'What do you think might help become more confident in making a change?'
+        }
+      ]
+    },
+]
   return (
     <View
       lightColor='#f5f5f5'
@@ -23,9 +78,8 @@ const QuestionsListScreen = ({ navigation, route }) => {
       width: '100%',
       paddingTop: '10%'
     }}>
-      <View style={{ width: '90%', marginLeft: '5%', marginBottom: '5%' }}>
-        <SegmentedControl
-          tintColor='green'
+      <SegmentedControl
+          style={{width: '90%', marginLeft: '5%', marginBottom: '5%'}}
           backgroundColor={PlatformColor('systemGray6')}
           values={['All Questions', 'Answered']}
           selectedIndex={selectedIndex}
@@ -33,88 +87,16 @@ const QuestionsListScreen = ({ navigation, route }) => {
             setSelectedIndex(event.nativeEvent.selectedSegmentIndex)
           }}
         />  
-      </View>
       <SectionList
         keyExtractor={(item, index) => item + index}
-        sections={[
-          {
-            title: 'Intentions for change:',
-            data: [
-              {
-                text: 'What do you intend to do?'
-              },
-              {
-                text: 'What do you think you might be able to do?'
-              },
-            ]
-          },
-          {
-            title: 'Disadvantages of the status quo:',
-            data: [
-              {
-                text: 'What concerns you about your current situation?'
-              },
-              {
-                text: 'What do you think might happen if you do not change?'
-              }
-            ]
-          },
-          {
-            title: 'Expressing optimism:',
-            data: [
-              {
-                text: 'How confident are you that you can make this change?'
-              },
-              {
-                text: 'What kind of support would be helpful in making this change?'
-              },
-              {
-                text: 'What kind of support would be helpful in making this change?'
-              }
-            ]
-          },
-          {
-            title: 'Advantages of change:',
-            data: [
-              {
-                text: 'If you could wake up tomorrow and things changed by magic, how would things be better for you?'
-              }
-            ]
-          },
-          {
-            title: 'Scaling:',
-            data: [
-              {
-              text: 'What do you think might help become more confident in making a change?'
-              }
-            ]
-          },
-      ]}
+        sections={questionSectionData}
         renderSectionHeader={({ section: { title } }) => (
           <Text style={{backgroundColor: colorScheme === 'dark' ? PlatformColor('systemGray6') : '#f5f5f5', fontWeight: 'bold', paddingLeft: '5%', marginBottom: '4%', paddingBottom: '4%'}}>{title}</Text>
         )}
         renderItem={({ item }) => (
-          <Pressable
-            onPress={() => navigation.navigate('AnswerQuestionScreen', {questionText: item.text})}
-            style={{
-              borderRadius: 10, 
-              padding: '6%', 
-              backgroundColor: colorScheme === 'dark' ? PlatformColor('systemGray5') : '#f8fbf8',
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 1,
-              },
-              shadowOpacity: 0.20,
-              shadowRadius: 1.41,
-              elevation: 2,
-              width: '90%', 
-              marginLeft: '5%', 
-              marginBottom: '5%'
-            }}>
-            <Text>{item.text}</Text>
-          </Pressable>
-        )} />
+          <PressableTextCard text={item.text} onPress={() => navigation.navigate('AnswerQuestionScreen', { questionText: item.text })} />
+        )}
+      />
     </View>
   )
 }
