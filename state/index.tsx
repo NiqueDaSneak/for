9780f1a53554cbe1ff12287435ff294a673edb5e
niquedasneak <thisisdom4@gmail.com>
2021-04-06 18:20:35 -0400

    new context for index

--- state/index.tsx
+++ state/index.tsx
@@ -1,5 +1,5 @@
 import { AuthContext } from './auth.context'
 import { ModalContext } from './modal.context'
 import { DigitalThoughtsContext } from './digital-thoughts.context'
-
-export { AuthContext, ModalContext, DigitalThoughtsContext }
+import {AlignCategoriesContext} from './align-categories.context'
+export { AuthContext, ModalContext, DigitalThoughtsContext, AlignCategoriesContext }
