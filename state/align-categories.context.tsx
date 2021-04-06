b492aa8e9e5b6ba4fabae868a6aebe47295f547c
niquedasneak <thisisdom4@gmail.com>
2021-04-06 18:20:08 -0400

    new context for align categories

---
+++ state/align-categories.context.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import {
+  createContext,
+  useReducer
+} from 'react'
+
+export const AlignCategoriesContext = createContext()
+// I THINK THE DIFFERENCE BETWEEN THIS AND THE DIGITAL THOUGHT
+// CONTEXT IS THIS ONE IS ABOUT THE ORGANIZATION OF THE DATA FOR
+// THE CUSTOMER
+
+const initialState = {
+  categories: [{title: 'Health & Wellness'}, {title: 'Relationships'}, {title: 'Finances'}],
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case '_':
+      return {
+      ...state
+      }
+      default:
+        throw new Error()
+  }
+}
+
+export const AlignCategoriesProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <AlignCategoriesContext.Provider value={[state, dispatch]}>
+      {children}
+    </AlignCategoriesContext.Provider>
+  )
+  
+}
+
