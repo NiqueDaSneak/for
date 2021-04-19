8df71490bd11f13e509321d5f2a51281d7479831
niquedasneak <thisisdom4@gmail.com>
2021-04-19 11:11:19 -0400

    add close and prettier

--- components/modals/ModalConductor.tsx
+++ components/modals/ModalConductor.tsx
@@ -1,34 +1,34 @@
-import React, {useContext} from 'react'
-import { ModalContext } from '../../state'
-import PhoneVerification from './PhoneVerification'
-import NewUserHelpModal from './NewUserHelpModal'
-import GoalCreationModal from './GoalCreationModal'
+import React, { useContext } from 'react';
+import { ModalContext } from '../../state';
+import PhoneVerification from './PhoneVerification';
+import NewUserHelpModal from './NewUserHelpModal';
+import GoalCreationModal from './GoalCreationModal';
 
-const ModalConductor = ({colorScheme}: {colorScheme: string}) => {
-  const [state, dispatch] = useContext(ModalContext)
-  const {
-    modalType,
-    modalData,
-  } = state
+const ModalConductor = ({ colorScheme }: { colorScheme: string }) => {
+  const [state, dispatch] = useContext(ModalContext);
+  const { modalType, modalData } = state;
 
   const closeModal = () => {
-    dispatch({type: 'CLOSE'})
-  }
+    dispatch({ type: 'CLOSE' });
+  };
   return (
     <>
       <PhoneVerification
         visible={modalType === 'GET_PHONE'}
         colorScheme={colorScheme}
-        close={() => closeModal()} />
+        close={() => closeModal()}
+      />
       <NewUserHelpModal
         visible={modalType === 'SHOW_NEW_HELP'}
         colorScheme={colorScheme}
-        close={() => closeModal()} />
+        close={() => closeModal()}
+      />
       <GoalCreationModal
         visible={modalType === 'CREATE_GOAL'}
-        />
+        close={closeModal}
+      />
     </>
-  )
- }
+  );
+};
 
-export default ModalConductor
+export default ModalConductor;
