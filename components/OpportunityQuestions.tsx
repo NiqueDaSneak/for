4f7008b8a3dbf05e0859f708a4db72e4ffb13bbc
niquedasneak <thisisdom4@gmail.com>
2021-05-05 12:15:55 -0400

    firebase and prettier

--- components/OpportunityQuestions.tsx
+++ components/OpportunityQuestions.tsx
@@ -1,48 +1,93 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 import {
   PlatformColor,
-  useColorScheme,
-  View as ContainerView,
 } from 'react-native';
-import { ScrollView, TextInput } from 'react-native-gesture-handler';
-import { useFonts } from '../hooks/useFonts';
+import { ScrollView } from 'react-native-gesture-handler';
 import { useKeyboard } from '../hooks/useKeyboard';
-import { Question } from '../state/opportunities.context';
-import TextCard from './TextCards/TextCard';
+import {  Question } from '../state/opportunities.context';
 import { Text } from './Themed';
 import ToDoList from './ToDoList';
 import HowWouldYou from './HowWouldYou';
 import ZeroTo100 from './ZeroTo100';
 
-const OpportunityQuestions = ({ questions }: { questions: Question[] }) => {
-  const colorScheme = useColorScheme();
-  console.log('questions: ', questions);
-  const [text, setText] = useState('');
-  const { fontTypes, fontSizes } = useFonts();
+const OpportunityQuestions = ({
+  questions,
+}: {
+  opportunityId: string;
+  questions: Question[];
+}) => {
   const { keyboardHeight } = useKeyboard();
+
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop: '10%',
+      height: '70%',
+      width: '100%',
+      alignItems: 'center',
+    },
+    instructions: {
+      paddingTop: '10%',
+      width: '80%',
+    },
+    scrollContainer: {
+      width: '100%',
+      paddingTop: '10%',
+      paddingBottom: keyboardHeight > 0 ? '100%' : '30%',
+      minHeight: '100%',
+    },
+  });
   return (
-    <ScrollView>
-      {questions?.filter((q) => q.questionType === 'BEST_TO_DO')
-        .map((question) => (
-          <ToDoList questionData={question} />
-        ))}
+    <>
+      {questions.length === 0 ? (
+        <View style={styles.container}>
+          <Image
+            resizeMode="contain"
+            resizeMethod="resize"
+            style={{
+              resizeMode: 'contain',
+              height: 190,
+              width: '100%',
+            }}
+            source={require('../assets/images/opportunity-explain.png')}
+          />
 
-      {questions?.filter((q) => q.questionType === 'BEST_NOT_TO_DO')
-        .map((question) => (
-          <ToDoList questionData={question} />
-        ))}
+          <Text style={styles.instructions}>
+            Tap the button below to add question modules to your opportunity.
+            Question modules allow you to refine this opportunity into something
+            to reflect on or a goal.
+          </Text>
+        </View>
+      ) : (
+        <View lightColor="#f5f5f5" darkColor={PlatformColor('systemGray6')}>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            {questions
+              .filter((q: Question) => q.questionType === 'BEST')
+              .map((question: Question) => (
+                <ToDoList key={question.id} question={question} />
+              ))}
 
-      {questions?.filter((q) => q.questionType === 'COUNTER')
-        .map((question) => (
-          <ZeroTo100/>
-        ))}
+            {questions
+              .filter((q: Question) => q.questionType === 'NOT_BEST')
+              .map((question: Question) => (
+                <ToDoList key={question.id} question={question} />
+              ))}
 
-      {questions?.filter((q) => q.questionType === 'HOW_WOULD')
-        .map((question) => (
-          <HowWouldYou />
-        ))}
-    </ScrollView>
+            {questions
+              .filter((q: Question) => q.questionType === 'COUNTER')
+              .map((question) => (
+                <ZeroTo100 key={question.id} question={question} />
+              ))}
+
+            {questions
+              ?.filter((q: Question) => q.questionType === 'HOW_WOULD')
+              .map((question) => (
+                <HowWouldYou key={question.id} question={question} />
+              ))}
+          </ScrollView>
+        </View>
+      )}
+    </>
   );
 };
 
